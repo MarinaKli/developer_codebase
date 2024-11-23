50254483ce32a8c856af9b008c60d0685ff6b860
Marina Klimenko <marina@Marinas-MacBook-Pro.local>
2024-11-23 15:51:38 -0800

    commit3

--- handson-developer-main/nodejs-project/tests/index.test.js
+++ handson-developer-main/nodejs-project/tests/index.test.js
@@ -1,9 +1,6 @@
-const { add, multiply } = require('../src/index');
+const add = require('../src/index');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
 
-test('multiplies 2 * 3 to equal 7', () => {
-     expect(multiply(2, 3)).toBe(7);
-});
